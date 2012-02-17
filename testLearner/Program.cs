05ab8722446468435fcdd2e864969ca94f6ffc3a
Jeffrey Crowell <crowell@bu.edu>
2012-02-17 10:34:54 -0500

    updated the tester to have a much better example

--- testLearner/Program.cs
+++ testLearner/Program.cs
@@ -10,30 +10,50 @@ namespace testLearner
     {
         static void Main(string[] args)
         {
-            int[] layers = { 2, 2, 6, 2 };
+            int[] layers = { 4, 6, 4 };
             SMiLe.NeuralNet net = new NeuralNet(layers);
             List<List<double>> inputvs2 = new List<List<double>>();
-            inputvs2.Add(new List<double>() { 0, 0 });
-            inputvs2.Add(new List<double>(){ 0, 1 });
-            inputvs2.Add(new List<double>(){ 1, 1 });
-           // inputvs2.Add(new List<double>() { 1, 0 });
+            inputvs2.Add(new List<double>() { 0, 0, 0, 0 });
+            inputvs2.Add(new List<double>(){ 0, 0, 0, 1 });
+            inputvs2.Add(new List<double>(){ 0,0,1, 0 });
+            inputvs2.Add(new List<double>() { 0, 0, 1, 1 });
+            inputvs2.Add(new List<double>() { 0, 1, 0, 0 });
+            inputvs2.Add(new List<double>() { 0, 1, 0, 1 });
+            inputvs2.Add(new List<double>() { 0, 1, 1, 1 });
+            inputvs2.Add(new List<double>() { 1, 0, 0, 0 });
+            inputvs2.Add(new List<double>() { 1, 0, 0, 1 });
+            //inputvs2.Add(new List<double>() { 1, 0, 1, 0 });
+            inputvs2.Add(new List<double>() { 1, 0, 1, 1 });
+            inputvs2.Add(new List<double>() { 1, 1, 0, 0 });
+            inputvs2.Add(new List<double>() { 1, 1, 0, 1 });
+            inputvs2.Add(new List<double>() { 1, 1, 1, 1 });
             
             List<List<double>> outputvs2 = new List<List<double>>();
-            outputvs2.Add(new List<double>() { 0, 0 });
-            outputvs2.Add(new List<double>(){ 0, 1 });
-            outputvs2.Add(new List<double>(){ 1, 1 });
-           // outputvs2.Add(new List<double>() { 1, 0 });
+            outputvs2.Add(new List<double>() { 0, 0, 0, 0 });
+            outputvs2.Add(new List<double>() { 0, 0, 0, 1 });
+            outputvs2.Add(new List<double>() { 0, 0, 1, 0 });
+            outputvs2.Add(new List<double>() { 0, 0, 1, 1 });
+            outputvs2.Add(new List<double>() { 0, 1, 0, 0 });
+            outputvs2.Add(new List<double>() { 0, 1, 0, 1 });
+            outputvs2.Add(new List<double>() { 0, 1, 1, 1 });
+            outputvs2.Add(new List<double>() { 1, 0, 0, 0 });
+            outputvs2.Add(new List<double>() { 1, 0, 0, 1 });
+            //outputvs2.Add(new List<double>() { 1, 0, 1, 0 });
+            outputvs2.Add(new List<double>() { 1, 0, 1, 1 });
+            outputvs2.Add(new List<double>() { 1, 1, 0, 0 });
+            outputvs2.Add(new List<double>() { 1, 1, 0, 1 });
+            outputvs2.Add(new List<double>() { 1, 1, 1, 1 });
+
             
 
             List<List<double>> testing = new List<List<double>>();
             List<List<double>> testout = new List<List<double>>();
-            testing.Add(new List<double>() { 1, 0 });
-            testout.Add(new List<double>() { 1, 0 });
+            testing.Add(new List<double>() {1, 0, 1, 0 });
 
 
-            for (int ii = 0; ii < 10; ii++)
+            for (int ii = 0; ii < 100; ii++)
             {
-                net.train(inputvs2, outputvs2, 3);
+                net.train(inputvs2, outputvs2, 2);
                 System.Console.WriteLine(net.error(inputvs2, outputvs2));
             }
 
@@ -45,6 +65,8 @@ namespace testLearner
             NeuralNet net2 = new NeuralNet(layers);
             net2 = net2.LOAD("neural.net");
             NeuralNet net34 = new NeuralNet().LOAD("neural.net");
+            List<double> testing2 = new List<double>() { 0, 0, 1, 0 };
+            output = net34.evaluate(testing2);
 
             //System.Console.WriteLine(net.error(testing, testout));
 
